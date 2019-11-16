a38ca7801f286d8eb6af7fe5b4f13f4ac148c3cb
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-11-15 18:00:05 -0700

    Created cards that shuffle and update boolean when clicked

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ImageCard from "./components/ImageCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import cards from "./images.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.cards to the cards json array
+  state = {
+    cards,
+    score: 0
+  };
+
+  updateScore = () => {
+    this.setState({ score: this.state.score + 1 });
+    console.log(this.state.score);
+  }
+
+  randomizeCards = id => {
+    const updateCards = this.state.cards
+      .map(onecard => { 
+        if (onecard.id === id) {
+          onecard.clicked = true;
+          this.updateScore();
+          return onecard;
+        }
+          return onecard; 
+        });
+
+    const cards = updateCards.map((a) => [Math.random(), a]).sort((a, b) => a[0] - b[0]).map((a) => a[1]);
+    this.setState({ cards });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Click Around</Title>
+        {this.state.cards.map(card => (
+          <ImageCard
+          randomizeCards={this.randomizeCards}
+            id={card.id}
+            key={card.id}
+            name={card.name}
+            image={card.image}
+            clicked={card.clicked}
+
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
+
---
+++ src/components/ImageCard/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+
+function ImageCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          {/* <li>
+            <strong>Occupation:</strong> {props.occupation}
+          </li>
+          <li>
+            <strong>Location:</strong> {props.location}
+          </li> */}
+        </ul>
+      </div>
+      <span onClick={() => props.randomizeCards(props.id)} className="remove">
+        Click
+      </span>
+    </div>
+  );
+}
+
+export default ImageCard;
